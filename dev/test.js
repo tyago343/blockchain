32c2d7d183f00ff347bfd15ce6fcc231507e157d
Santiago Andres Casanova <tyagocasanova@hotmail.com>
2019-07-27 11:30:02 +0200

    first commit

---
+++ dev/test.js
@@ -0,0 +1,7 @@
+const Blockchain = require ('./blockchain.js');
+const bitcoin = new Blockchain;
+bitcoin.createNewBlock(2345, 'OJANDVGVNV09', 'OSDNAS0DV9USD9V8H');
+bitcoin.createNewBlock(111, 'fuasdhf9asd8f', '8ufsd0aaf0sad9fh');
+bitcoin.createNewBlock(1312, 'd9ufya87dsfya7', '9f87ady987adf98');
+
+console.log(bitcoin);
