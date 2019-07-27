b72257af3f0e8941d9c2f5a948ecd214685079be
Santiago Andres Casanova <tyagocasanova@hotmail.com>
2019-07-27 13:06:17 +0200

    new functionalities for new transactions blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,18 +1,30 @@
 function Blockchain () {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 };
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transcations: this.newTransactions,
+        transcations: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
     return newBlock;
 };
+Blockchain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length -1];
+}
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount,
+        sender,
+        recipient
+    };
+    this.pendingTransactions.push(newTransaction);
+    return this.getLastBlock()['index'] + 1;
+}
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,7 +1,11 @@
 const Blockchain = require ('./blockchain.js');
 const bitcoin = new Blockchain;
-bitcoin.createNewBlock(2345, 'OJANDVGVNV09', 'OSDNAS0DV9USD9V8H');
-bitcoin.createNewBlock(111, 'fuasdhf9asd8f', '8ufsd0aaf0sad9fh');
-bitcoin.createNewBlock(1312, 'd9ufya87dsfya7', '9f87ady987adf98');
+
+bitcoin.createNewBlock(1, 'nonsencehashes', 'newhash');
+bitcoin.createNewTransaction(200, 'Santiagoaddress', 'Amaliaaddress');
+bitcoin.createNewBlock(123123, 'testesetes', '09das0f');
+bitcoin.createNewTransaction(5000, 'Santiagoaddress', 'Amaliaaddress');
+bitcoin.createNewTransaction(20, 'Santiagoaddress', 'Amaliaaddress');
+bitcoin.createNewTransaction(412, 'Santiagoaddress', 'Amaliaaddress');
 
 console.log(bitcoin);
