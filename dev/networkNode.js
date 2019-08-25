5b30ad7ff7b3358eebb98e1509fd0eb33a431b5c
Santiago Casanova <tyagocasanova@hotmail.com>
2019-08-25 16:41:57 +0200

    adding a visual block explorer

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -226,9 +226,15 @@ app.get('/transaction/:transactionId', function(req, res){
 
 
 app.get('/address/:address', function(req, res){
-
+    const address = req.params.address;
+    const addressData = bitcoin.getAddressData(address);
+    res.json(addressData);
 });
 
+
+app.get('/block-explorer', function(req, res){
+    res.sendFile('./block-explorer/index.html', { root: __dirname});
+})
 
 app.listen(port, function(){
     console.log(`Server listening on port: ${port}`);
